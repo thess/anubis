c2cc1df17252f3ecd52751b711ccba351fffe90a
Xe Iaso <xe.iaso@techaro.lol>
2025-07-14 10:01:03 -0400

    test: add smoke test for git clone (#828)
    * test: add smoke test for git clone
    
    Signed-off-by: Xe Iaso <me@xeiaso.net>
    
    * chore: exempt tests from spelling
    
    Signed-off-by: Xe Iaso <me@xeiaso.net>
    
    * test: rename this to git-clone
    
    Signed-off-by: Xe Iaso <me@xeiaso.net>
    
    * test: pin ko setup reference
    
    Signed-off-by: Xe Iaso <me@xeiaso.net>
    
    * test: don't persist credentials
    
    Signed-off-by: Xe Iaso <me@xeiaso.net>
    
    * test: terminating newline
    
    Signed-off-by: Xe Iaso <me@xeiaso.net>
    
    ---------
    
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ test/git-clone/test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+export VERSION=$GITHUB_COMMIT-test
+export KO_DOCKER_REPO=ko.local
+
+set -u
+
+(
+  cd ../.. && \
+  ko build --platform=all --base-import-paths --tags="latest" --image-user=1000 --image-annotation="" --image-label="" ./cmd/anubis -L
+)
+
+rm -rf ./var/repos ./var/clones
+mkdir -p ./var/repos ./var/clones
+
+(cd ./var/repos && git clone --bare https://github.com/TecharoHQ/status.git)
+
+docker compose up -d
+
+sleep 2
+
+(cd ./var/clones && git clone http://localhost:3000/status.git)
+
+docker compose down
